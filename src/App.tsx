23bd618dcb4fff317870138826775265803bec0d
Shahin-Mia <shahin.cmt2@gmail.com>
2024-07-06 00:14:07 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './components/header/Header'
+import Navbar from './components/header/Navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
